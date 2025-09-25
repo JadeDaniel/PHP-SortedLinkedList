1d57b766fed4d9847b8553f2cb0db4015c5bcc6c
Jade Steffen <steffenjade@gmail.com>
2025-09-24 21:02:55 -0500

    Add Test

--- tests/SortedLinkedListTest.php
+++ tests/SortedLinkedListTest.php
@@ -1,7 +1,32 @@
 <?php
+namespace Jade;
 
-namespace test;
-class SortedLinkedListTest
+use PHPUnit\Framework\TestCase;
+
+class SortedLinkedListTest extends TestCase
 {
 
+    public function testHappyPath(): void
+    {
+        $list = new SortedLinkedList();
+        $list->add(new IntNode(10));
+        echo $list;
+        echo "\n";
+
+        $list->add(new IntNode(4));
+        echo $list;
+        echo "\n";
+
+        $list->add(new IntNode(6));
+        echo $list;
+        echo "\n";
+
+        $list->add(new IntNode(500));
+        echo $list;
+        echo "\n";
+
+        $list->add(new IntNode(350));
+        echo $list;
+        echo "\n";
+    }
 }
